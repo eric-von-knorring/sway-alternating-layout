4e8e4d66c3c2d3c7f98bba8395c2de3935fcfb74
Vonk <vonk@placeholder.mail>
2023-07-10 00:12:55 +0200

    Initial implementation

---
+++ src/alternating_layout.rs
@@ -0,0 +1,63 @@
+use std::process;
+
+use swayipc::{Connection, Event, EventStream, EventType, Fallible, Node, NodeLayout, WindowChange};
+
+pub fn start_alternating_layout(){
+    let (subscription, mut command) = connect();
+
+    for event in subscription {
+        handle_event(event, &mut command);
+    };
+}
+
+fn connect() -> (EventStream, Connection) {
+    let subscription = match Connection::new()
+        .and_then(|connection| connection.subscribe([EventType::Window])) {
+        Ok(conn) => conn,
+        Err(error) => {
+            eprintln!("Error: failed to set upp ipc subscription. {}", error);
+            process::exit(exitcode::IOERR);
+        }
+    };
+
+    let commands = match Connection::new() {
+        Ok(conn) => conn,
+        Err(error) => {
+            eprintln!("Error: failed to set upp ipc connection. {}", error);
+            process::exit(exitcode::IOERR);
+        }
+    };
+
+    (subscription, commands)
+}
+
+fn handle_event(event: Fallible<Event>, command: &mut Connection) {
+    let Ok(Event::Window(window)) = event else {
+        return;
+    };
+    if WindowChange::Focus == window.change {
+        set_layout(window.container, command);
+    };
+}
+
+fn set_layout(focused: Node, command: &mut Connection) {
+   if let Ok(Some(parent)) = command.get_tree().map(|tree| tree.find(|node| node.nodes.contains(&focused))) {
+       if NodeLayout::Tabbed == parent.layout || NodeLayout::Stacked == parent.layout {
+           return;
+       }
+    };
+
+    let Ok(Some(focused_current)) = command.get_tree().map(|tree| tree.find(|node| node.id == focused.id)) else {
+        eprintln!("Warning: Could not find refresh focused node.");
+        return;
+    };
+    if focused_current.rect.height > focused_current.rect.width {
+        if let Err(error) = command.run_command("splitv") {
+            eprintln!("Error: Failed to run splitv. {}", error)
+        };
+    } else {
+        if let Err(error) = command.run_command("splith") {
+            eprintln!("Error: Failed to run splith. {}", error)
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+mod alternating_layout;
+
+extern crate exitcode;
+
+use std::process;
+use crate::alternating_layout::start_alternating_layout;
+
 fn main() {
-    
+    let mut args = std::env::args().skip(1);
+
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-p" => {
+                let path = args.next()
+                    .unwrap_or_else(|| {
+                        eprintln!("Error: missing path to pid file");
+                        process::exit(exitcode::USAGE);
+                    });
+                write_pid(path)
+            },
+            "-h" => print_help_text(),
+            _ => unknown_argument(&arg)
+        }
+    }
+
+    start_alternating_layout();
+}
+
+fn print_help_text() {
+    println!("Usage: sway-alternating-layout [-p path/to/pid.file]
+
+Options:
+    -p path/to/pid.file   Saves the PID for this program in the filename specified")
+}
+
+fn unknown_argument(arg: &str) {
+    eprintln!("Error: Unknown argument '{}'", arg);
+    process::exit(exitcode::USAGE);
+}
+
+fn write_pid(path: String) {
+    match std::fs::write(path, process::id().to_string()) {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Error: could not write pid. '{}'", err);
+            process::exit(exitcode::CANTCREAT);
+        }
+    }
 }
