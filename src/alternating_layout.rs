b8d0d498ef16c00c17a32e0694b6cd2d1cc6c93c
Vonk <vonk@placeholder.mail>
2023-07-10 12:08:08 +0200

    Fixed tabbed layout

--- src/alternating_layout.rs
+++ src/alternating_layout.rs
@@ -2,7 +2,7 @@ use std::process;
 
 use swayipc::{Connection, Event, EventStream, EventType, Fallible, Node, NodeLayout, WindowChange};
 
-pub fn start_alternating_layout(){
+pub fn start_alternating_layout() {
     let (subscription, mut command) = connect();
 
     for event in subscription {
@@ -41,23 +41,29 @@ fn handle_event(event: Fallible<Event>, command: &mut Connection) {
 }
 
 fn set_layout(focused: Node, command: &mut Connection) {
-   if let Ok(Some(parent)) = command.get_tree().map(|tree| tree.find(|node| node.nodes.contains(&focused))) {
-       if NodeLayout::Tabbed == parent.layout || NodeLayout::Stacked == parent.layout {
-           return;
-       }
-    };
+    if let Ok(Some(parent)) = command.get_tree().map(|tree| find_parent(&focused, tree)) {
+        if NodeLayout::Tabbed == parent.layout || NodeLayout::Stacked == parent.layout {
+            return;
+        }
+    } else {
+        eprintln!("Warning: Could not find parent.");
+    }
 
     let Ok(Some(focused_current)) = command.get_tree().map(|tree| tree.find(|node| node.id == focused.id)) else {
         eprintln!("Warning: Could not find refresh focused node.");
         return;
     };
     if focused_current.rect.height > focused_current.rect.width {
-        if let Err(error) = command.run_command("splitv") {
+        if let Err(error) = command.run_command(format!("[con_id={}] splitv", focused_current.id)) {
             eprintln!("Error: Failed to run splitv. {}", error)
         };
     } else {
-        if let Err(error) = command.run_command("splith") {
+        if let Err(error) = command.run_command(format!("[con_id={}] splith", focused_current.id)) {
             eprintln!("Error: Failed to run splith. {}", error)
         };
     }
+}
+
+fn find_parent(focused: &Node, tree: Node) -> Option<Node> {
+    tree.find(|node| node.nodes.iter().any(|child| child.id == focused.id))
 }
